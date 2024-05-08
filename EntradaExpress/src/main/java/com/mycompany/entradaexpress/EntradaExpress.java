74cba1d37bc086b63e2c44815e6b1a2c11529252
Fernanda Martins Viotto <140853127+Fernandamviotto@users.noreply.github.com>
2024-05-08 19:16:49 -0300

    criacao

---
+++ EntradaExpress/src/main/java/com/mycompany/entradaexpress/EntradaExpress.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.entradaexpress;
+
+/**
+ *
+ * @author fernanda.silva
+ */
+public class EntradaExpress {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
